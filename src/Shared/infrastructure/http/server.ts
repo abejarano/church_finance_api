5300ee98c2082f9d2646c3e711e87ababa3eba1c
abejarano <programador.angel@gmail.com>
2025-01-12 20:37:00 -0300

    fix: rateLimit and bull

--- src/Shared/infrastructure/http/server.ts
+++ src/Shared/infrastructure/http/server.ts
@@ -25,6 +25,10 @@ export function server(port: number) {
     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    skip: (req) => {
+      // Excluye las rutas de @bull-board/express
+      return req.baseUrl.startsWith("/admin/queues");
+    },
   });
 
   app.use(limiter);
