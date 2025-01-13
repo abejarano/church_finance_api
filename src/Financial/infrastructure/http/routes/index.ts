947a4540d3ee9c35d1158c9ebd8a3b5281f7a412
abejarano <programador.angel@gmail.com>
2025-01-13 08:15:39 -0300

    fix: rateLimit and bull

--- src/Financial/infrastructure/http/routes/index.ts
+++ src/Financial/infrastructure/http/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import financialConfigurationRoute from "./FinancialConfiguration.routes";
-import memberContributionsRoutes from "./mobileApp/MemberContribution.routes";
 import financeRoute from "./Finance.route";
 
 const financialRouter = Router();
 
-financialRouter.use("/member-contributions", memberContributionsRoutes);
 financialRouter.use("/configuration", financialConfigurationRoute);
 financialRouter.use("/", financeRoute);
 
